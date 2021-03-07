efd801813907245be6fe563d87d1214611618374
Ofir Luson <ofir@support-1.lan>
2021-03-07 09:00:51 +0200

    check

--- src/Components/SharedArea/Page404/Page404.tsx
+++ src/Components/SharedArea/Page404/Page404.tsx
@@ -4,7 +4,7 @@ function Page404(): JSX.Element {
     return (
         <div className="Page404">
             <p>The page you are looking for doesn't exist.</p>
-
+            <h1></h1>
             <iframe width="560" height="315" src="https://www.youtube.com/embed/t3otBjVZzT0?autoplay=1" allow="autoplay" title="Page not Found"></iframe>
 
         </div>
